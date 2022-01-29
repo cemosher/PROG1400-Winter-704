87b8cb8a904935962b879ae8b0d6199cb809550e
cemosher <89281185+cemosher@users.noreply.github.com>
2022-01-29 15:02:44 -0400

    New Hello World project

--- Demos/HelloWorld/src/com/company/Main.java
+++ Demos/HelloWorld/src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
+        System.out.println("Hi from Github");
     }
 }
